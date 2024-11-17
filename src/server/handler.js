d86c5b0519b517d09ca96dc7a7af9dc41399ba54
Anggit Nur Ridho <91199758+nuridho-anggit@users.noreply.github.com>
2024-11-18 01:12:14 +0700

    adding fail response

--- src/server/handler.js
+++ src/server/handler.js
@@ -8,7 +8,6 @@ const MAX_FILE_SIZE = 1000000; // 1MB
 async function postPredictHandler(request, h) {
     const { image } = request.payload;
     
-    // Memeriksa ukuran file gambar
     if (Buffer.byteLength(image, 'base64') > MAX_FILE_SIZE) {
         const response = h.response({
             status: 'fail',
@@ -21,10 +20,9 @@ async function postPredictHandler(request, h) {
     const { model } = request.server.app;
 
     try {
-        // Melakukan prediksi
+        
         const { label_final, suggestion } = await predictClassification(model, image);
         
-        // Membuat ID dan data prediksi
         const id = crypto.randomUUID();
         const createdAt = new Date().toISOString();
 
@@ -35,10 +33,8 @@ async function postPredictHandler(request, h) {
             "createdAt": createdAt
         };
 
-        // Menyimpan data ke database Firestore
         await storeData(id, data);
 
-        // Mengembalikan response sukses
         const response = h.response({
             status: 'success',
             message: 'Model is predicted successfully',
@@ -48,7 +44,7 @@ async function postPredictHandler(request, h) {
         return response;
 
     } catch (error) {
-        // Jika terjadi kesalahan saat melakukan prediksi (seperti format gambar yang salah)
+        
         const response = h.response({
             status: 'fail',
             message: 'Terjadi kesalahan dalam melakukan prediksi'
